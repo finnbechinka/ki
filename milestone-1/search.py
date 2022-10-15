75f9483545eaef9db88ba5cf2d587c22b8f8c9aa
Finn <contact@bechinka.eu>
2022-10-15 22:51:29 +0200

    feat: add depth-first sort

--- milestone-1/search.py
+++ milestone-1/search.py
@@ -1,3 +1,6 @@
+from platform import node
+
+
 nodes = [
     "Augsburg",
     "Erfurt",
@@ -29,3 +32,32 @@ for i in range(len(nodes)):
 
 for i in range(len(edges)):
     print(f"{nodes[edges[i][0]]} -> {nodes[edges[i][1]]}")
+
+
+def index_to_name(list):
+    names = []
+    for i in list:
+        names.append(nodes[i])
+    return names
+
+
+def dfs(start=8, end=9):
+    visited = []
+    stack = [start]
+    while len(stack) > 0:
+        print(f"--\n{index_to_name(stack)}, {index_to_name(visited)}\n--\n")
+        if stack[0] == end:
+            return "found"
+        visited.append(stack[0])
+        for e in edges:
+            if e[0] == stack[0]:
+                if e[1] not in visited:
+                    stack.insert(1, e[1])
+            elif e[1] == stack[0]:
+                if e[0] not in visited:
+                    stack.insert(1, e[0])
+        stack.pop(0)
+    return "unable to find"
+
+
+print(dfs())
