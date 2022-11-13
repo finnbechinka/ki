d7a7fd2f8371b0741cdea63fa17ab33e3ed3decd
Finn <contact@bechinka.eu>
2022-11-13 21:11:36 +0100

    feat:

--- milestone-1/poster/solution.py
+++ milestone-1/poster/solution.py
@@ -3,33 +3,43 @@ import random
 
 rooms = ["B 50-H", "B 70-H", "H 10-H", "H 107", "D 317", "D 318", "D 319", "D 320", "D 327", "D 328", "A 250-H", "J 101"]
 days = [0, 1, 2, 3, 4]
-times = [8, 10, 12, 14, 16]
+times = [8, 10, 12]
 
 dt = [(x, y) for x in days for y in times]
 slots = [(x, y) for x in rooms for y in dt]
 
 lectures = [
-    ("Mathe 1", 1),
-    ("Einf.i.d.Programm.m.Skriptsprachen", 1),
-    ("techn. Informatik", 1),
-    ("Einführung i. d. Informatik", 1),
-    ("OOP", 1),
-    ("asdasd1", 1),
-    ("Einfasdasdhen", 1),
-    ("dteasdasdik", 1),
-    ("Eiasdasdasik", 1),
-    ("sdasdasd", 1),
-    ("Software Engineering", 3),
-    ("Embedded Systems", 3),
-    ("Datenbanken", 3),
-    ("Systemprogrammierung", 3),
-    ("Software Projektmanagement", 3),
-    ("Sicherheit u. Zuverlässigkeit", 5),
-    ("Webengineering", 5),
-    ("Compilerbau", 5),
-    ("Künstl. Intelligent", 5),
-    ("Technical English", 5),
-    ("Computer Vision", 5),
+    ("Mathe 1/V1", 1, "George, BC"),
+    ("Mathe 1/V2", 1, "George, BC"),
+    ("Mathe 1/Ü", 1, "Kreienkamp, Angela"),
+    ("Einf.i.d.Programm.m.Skriptsprachen/V", 1, "George, BC"),
+    ("Einf.i.d.Programm.m.Skriptsprachen/P", 1, "George, BC"),
+    ("techn. Informatik/V", 1, "Kreienkamp, Angela"),
+    ("techn. Informatik/P", 1, "Kreienkamp, Angela"),
+    ("Einführung i. d. Informatik/V", 1, "Behrens, Grit"),
+    ("Einführung i. d. Informatik/Ü", 1, "Behrens, Grit"),
+    ("OOP/V", 1, "Rexilius, Jan"),
+    ("OOP/P", 1, "Rexilius, Jan"),
+    ("Software Engineering/V", 3, "Brunsmann, Jörg"),
+    ("Software Engineering/P", 3, "Brunsmann, Jörg"),
+    ("Embedded Systems/V", 3, "König, Matthias"),
+    ("Embedded Systems/P", 3, "König, Matthias"),
+    ("Datenbanken/V", 3, "Becking, Dominic"),
+    ("Datenbanken/P", 3, "Becking, Dominic"),
+    ("Systemprogrammierung/V", 3, "Gips, Carsten"),
+    ("Systemprogrammierung/P", 3, "Gips, Carsten"),
+    ("Software Projektmanagement", 3, "Teich, Irene"),
+    ("Sicherheit u. Zuverlässigkeit/V", 5, "Thiel, Christoph"),
+    ("Sicherheit u. Zuverlässigkeit/P", 5, "Thiel, Christoph"),
+    ("Webengineering/V", 5, "Behrens, Grit"),
+    ("Webengineering/P", 5, "Behrens, Grit"),
+    ("Compilerbau/V", 5, "Gips, Carsten"),
+    ("Compilerbau/P", 5, "Gips, Carsten"),
+    ("Künstl. Intelligent/V", 5, "Gips, Carsten"),
+    ("Künstl. Intelligent/P", 5, "Gips, Carsten"),
+    ("Technical English", 5, "Demir, Hüseyin"),
+    ("Computer Vision/V", 5, "Rexilius, Jan"),
+    ("Computer Vision/P", 5, "Rexilius, Jan"),
 ]
 
 
@@ -39,7 +49,7 @@ csp["values"] = []
 for i in range(len(csp["variables"])):
     tmp = slots.copy()
     random.shuffle(tmp)
-    csp["values"].append(tmp)
+    csp["values"].append(slots.copy())
 
 
 def complete(assignment):
@@ -51,19 +61,26 @@ def complete(assignment):
 def consistent(value, var, assignment, csp):
     free_days = {1: 0, 3: 4, 5: 2}
     sem = var[1]
+    p = var[2]
     dt = value[1]
     day = dt[0]
     time = dt[1]
     # check if day is free day for given semester
     if free_days[sem] == day:
         return False
-    # check if there is already a lecture at the proposed day/time for given semester
+    # check for time overlap
     for k, v in assignment.items():
         curr_sem = k[1]
+        curr_p = k[2]
         curr_dt = v[1]
         same_sem = curr_sem == sem
         same_dt = curr_dt == dt
+        same_p = curr_p == p
+        # check if sem already has event at given time
         if same_sem and same_dt:
+            return False
+        # check if prof already has event at given time
+        if same_dt and same_p:
             return False
     # check if slot is already is use
     if value in assignment.values():
